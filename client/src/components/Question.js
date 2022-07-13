444da45631e3b0b94f526440704fc6d03807341e
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-07-13 23:38:09 +0900

    22-07-13 many

---
+++ client/src/components/Question.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Question extends Component
+{
+  render()
+  {
+    return(
+      <div>
+        <h1>문제</h1>
+      </div>
+    );
+  }  
+}
+
+export default Question;
---
+++ client/src/components/QuestionBook.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class QuestionBook extends Component
+{
+  render()
+  {
+    return(
+      <div>
+        <h1>나의 문제집</h1>
+      </div>
+    );
+  }  
+}
+
+export default QuestionBook;
