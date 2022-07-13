e2deb01365969e2767a2a88f0b17b98648f9cd47
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-07-13 22:54:49 +0900

    22-07-13 main component ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World2
+      <div>
+        <h1>나의 문제</h1>
+        <div>
+          <span>사과의 스펠링은?</span>
+        </div>
+        <div>
+          <span>사과의 스펠링은?</span>
+        </div>
+        <div>
+          <span>사과의 스펠링은?</span>
+        </div>
+        <div>
+          <span>사과의 스펠링은?</span>
+        </div>
+        <div>
+          <span>사과의 스펠링은?</span>
+        </div>
+      </div>
     </div>
   );
 }
