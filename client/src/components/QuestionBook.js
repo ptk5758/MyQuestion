dec284fdf63c5da712752ac9188a739ae068a8f0
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-07-14 00:30:22 +0900

    22-07-14 작업중 커밋

--- client/src/components/QuestionBook.js
+++ client/src/components/QuestionBook.js
@@ -1,12 +1,40 @@
 import { Component } from 'react';
-
+import left_arrow from '../source/left-arrow.svg';
 class QuestionBook extends Component
 {
   render()
   {
     return(
-      <div>
-        <h1>나의 문제집</h1>
+      <div className='content'>
+        <div>
+            <div className='header'>
+                <div className='box-left'><img src={left_arrow}/></div>
+                <div className='title'>
+                    <span>문제집</span>
+                </div>
+                <div className='box-right'>&nbsp;</div>
+            </div>
+            <div className='questionbook'>
+                <div className='book'>
+                    <span>대충 책제목</span>
+                </div>
+                <div className='book'>
+                    <span>대충 책제목</span>
+                </div>
+                <div className='book'>
+                    <span>대충 책제목</span>
+                </div>
+                <div className='book'>
+                    <span>대충 책제목</span>
+                </div>
+                <div className='book'>
+                    <span>대충 책제목</span>
+                </div>
+                <div className='book'>
+                    <span>대충 책제목</span>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }  
