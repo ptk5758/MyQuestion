350b091b0b6e09efca70d8802342233952c68854
chrhfoq123 <chrhfoq123@naver.com>
2022-07-14 00:32:10 +0900

    question ui 구현

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -5,8 +5,30 @@ class Question extends Component
   render()
   {
     return(
-      <div>
-        <h1>문제</h1>
+      <div className='question'>
+        <div className='title'>
+          <span>&lt;</span>
+          <span>문제</span>
+        </div>
+        <hr/>
+        <div className='question_list'>
+          <p>사과의 스펠링으로 옳은 것은?</p>
+          <hr/>
+          <p>사과의 스펠링으로 옳은 것은?</p>
+          <hr/>
+          <p>사과의 스펠링으로 옳은 것은?</p>
+          <hr/>
+          <p>사과의 스펠링으로 옳은 것은?</p>
+          <hr/>
+          <p>사과의 스펠링으로 옳은 것은?</p>
+          <hr/>
+          <p>사과의 스펠링으로 옳은 것은?</p>
+          <hr/>
+        </div>
+        
+        <div className='button'>
+          <button className='regist_button'>등록하기</button>
+        </div>
       </div>
     );
   }  
