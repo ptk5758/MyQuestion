782adac6e29e30c8bf0fe0b367872dd2c8956dad
chrhfoq123 <chrhfoq123@naver.com>
2022-07-13 23:03:21 +0900

    questionbook 작업

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World2
+      <div>
+        <h1>문제집</h1>
+        <div>
+          <span>전공 과목 모음</span>
+        </div>
+        <div>
+          <span>전공 과목 모음</span>
+        </div>
+        <div>
+          <span>전공 과목 모음</span>
+        </div>
+        <div>
+          <span>전공 과목 모음</span>
+        </div>
+      </div>
     </div>
   );
 }
