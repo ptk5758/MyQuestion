b6728b142d591c58d4de646c771fc179a1279bee
chrhfoq123 <chrhfoq123@naver.com>
2022-07-15 23:58:37 +0900

    문제집 추가 ui 커밋

--- client/src/components/QuestionBookInsert.js
+++ client/src/components/QuestionBookInsert.js
@@ -9,6 +9,28 @@ class QuestionBookInsert extends Component
                 <div className='subject'>
                     <span className='title'>• 제목</span>
                 </div>
+                <div className='input'>
+                    <input className='input_title' type='text' placeholder='제목을 입력해주세요.'></input>
+                </div>
+                <div className='subject'>
+                    <span className='title'>• 현재 문제</span>
+                </div>
+                <div className='now_question'>
+                    <p className='question_list'>•사과 영어로 스펠링은?</p>
+                    <p className='question_list'>•사과 영어로 스펠링은?</p>
+                    <p className='question_list'>•사과 영어로 스펠링은?</p>
+                    <p className='question_list'>•사과 영어로 스펠링은?</p>
+                    <p className='question_list'>•사과 영어로 스펠링은?</p>
+                </div>
+                <div className='color_select'>
+                    <span className='title'>• 표지</span>
+                    <div className='color-list'>
+                        <p className='color'></p>
+                        <p className='color'></p>
+                        <p className='color'></p>
+                        <p className='color'></p>
+                    </div>
+                </div>
             </div>
         );
     }
