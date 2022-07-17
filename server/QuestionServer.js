30a52115a1373f2ea0bd645d62cc069b54f3601c
chrhfoq123 <chrhfoq123@naver.com>
2022-07-17 20:00:59 +0900

    database 추가

--- server/QuestionServer.js
+++ server/QuestionServer.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
+const df = fs.readFileSync('./database.json');
+const database = JSON.parse(df);
+
 let express = require("express");
+let mysql = require("mysql");
+let conn = mysql.createConnection({
+    host: database.host,
+    user: database.user,
+    password: database.password,
+    database: database.database,
+    port: database.port
+});
+
+conn.connect();
+
 let app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello world my server")
+    let q = "select * from question"
+    conn.query(q, (error, rows, field) => {
+        if(error)
+            console.log(error);
+        res.send(rows)
+    });
 });
 
 app.listen(5000, () =>{
